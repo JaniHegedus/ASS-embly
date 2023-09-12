86e9ca06e0db6114728c5a20f2ccf73485237408
Hegedüs János <hegedusjanos2002@gmail.com>
2023-09-12 22:00:13 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+0. Biztos, ami biztos, ez a mappa a C: gyökérkönyvtárban legyen
+1. Írd meg bármilyen szerkesztőben az asm kiterjesztésű fájlt
+2. Mentsd el ebbe a mappába
+3. Húzd rá (drag&drop) az asm fájlt a compile_run_masm.bat-ra, így lefut a kód doxboxot megnyitva
+4. Ne olvasd el a bat fájl kódját
+
+A 1_Conan és a 2_Toth_Gergo mappában vannak órai munkák és feladatmegoldások is.
+Nem biztos, hogy mindegyik működik és jó a megoldás.
---
+++ SPOILER_compile_run_masm.bat
@@ -0,0 +1,24 @@
+﻿echo off
+cls
+set inputPath=%~d1%~p1
+set inputPath=%inputPath:~0,-1%
+set inputFile=%~n1%~x1
+
+set objFile=%~n1.OBJ
+set mapFile=%~n1.MAP
+set executableFile=%~n1.EXE
+del %asmOutput%
+
+echo Assembling: MASM
+echo 	Input Path: %inputPath%
+echo 	Input File: %inputFile%
+
+echo Running DOSBox
+:: Gimesi, hogy basznám meg azt a jó édes kibaszot riherongy kurva anyádat a szőrös hátadon te hormonzavaros agykárosult lelkinyomorék
+:: down kóros fogyatékos balfasz a kibaszott 19-KIBASZOTT-82-es elcseszett programoddal. Ha ekkora fasz vagy, menj haza és hozogasd a pöcörődet a
+:: DOS-ra te majom, a leendő programozóket meg hagyjad már használni a szájbakúrt MODERN programokat.
+:: Utóirat: Te fasz.
+DosBOX\DOSbox.exe -c "mount C %inputPath%" -c "C:" -c "copy %inputFile% masm611\BIN\%inputFile%" -c "cd masm611\BIN" -c "del %executableFile%" -c "masm.exe %inputFile%" -c "link.exe %objFile%,%executableFile%,,,," -c "%executableFile%" -c "pause" -c "del %inputFile%" -c "del %objFile%" -c "del %mapFile%" -c "del %executableFile%" -noconsole -exit
+
+del stdout.txt
+del stderr.txt
